ed5e35cf5dd28acab57f1d7edf18130bfe401a6f
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-04-28 16:45:11 +0600

    Update Single.jsx

--- admin_panel/src/pages/single/Single.jsx
+++ admin_panel/src/pages/single/Single.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import "./single.scss";
+import Sidebar from "../../component/sidebar/Sidebar";
+import Navbar from "../../component/navbar/Navbar";
+import Chart from "../../component/chart/Chart";
+import Table from "../../component/table/Table";
 
- const Single = () => {
+const Single = () => {
   return (
-    <div>Single</div>
-  )
-}
-export default Single
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://picsum.photos/id/1005/300/300"
+                alt="avatar/"
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Jone Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">janedoe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+880 160 000 0000</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">Dhaka</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">Bangladesh</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
+          </div>
+        </div>
+        <div className="bottom">
+          <h1 className="title">Transactions</h1>
+          <Table />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Single;
