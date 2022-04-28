d58bbb1784e0afa2b36821f42f80a9f67e08d37b
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-04-28 16:45:06 +0600

    Update New.jsx

--- admin_panel/src/pages/new/New.jsx
+++ admin_panel/src/pages/new/New.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import "./new.scss"
+import Sidebar from "../../component/sidebar/Sidebar";
+import Navbar from "../../component/navbar/Navbar";
 const New = () => {
   return (
-    <div>New</div>
-  )
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1 className="title">Add User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              alt="image_icon"
+            />
+          </div>
+          <div className="right">right</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default New
