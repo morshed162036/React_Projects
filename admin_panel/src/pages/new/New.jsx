b1ec5a2ad58bcb07ab4b7059c2272e1bebaeb760
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-05-07 18:19:58 +0600

    Update New.jsx

--- admin_panel/src/pages/new/New.jsx
+++ admin_panel/src/pages/new/New.jsx
@@ -2,19 +2,22 @@ import "./new.scss"
 import Sidebar from "../../component/sidebar/Sidebar";
 import Navbar from "../../component/navbar/Navbar";
 import  DriveFolderUploadOutlinedIcon  from "@mui/icons-material/DriveFolderUploadOutlined";
-const New = () => {
+import { useState } from "react";
+const New = ({input,title}) => {
+
+  const [file, setFile] = useState("");
   return (
     <div className="new">
       <Sidebar />
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1 className="title">Add User</h1>
+          <h1 className="title">{title}</h1>
         </div>
         <div className="bottom">
           <div className="left">
             <img
-              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              src={file? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"}
               alt="image_icon"
             />
           </div>
@@ -24,32 +27,15 @@ const New = () => {
                 <label htmlFor="file">
                   Image: <DriveFolderUploadOutlinedIcon className="icon"/>
                 </label>
-                <input type="file" id="file" style={{display:'none'}}/>
+                <input type="file" id="file" onChange={e=>{setFile(e.target.files[0])}} style={{display:'none'}}/>
               </div>
-              <div className="formInput">
-                <label>UserName</label>
-                <input type="text" placeholder="john_doe" />
+              {input.map((input)=>(
+
+              <div className="formInput" key={input.id}>
+                <label>{input.label}</label>
+                <input type={input.type} placeholder={input.placeholder} />
               </div>
-              <div className="formInput">
-                <label>Name and Surname</label>
-                <input type="text" placeholder="John Doe" />
-              </div>
-              <div className="formInput">
-                <label>Phone</label>
-                <input type="text" placeholder="+880 123 4567" />
-              </div>
-              <div className="formInput">
-                <label>Password</label>
-                <input type="password" />
-              </div>
-              <div className="formInput">
-                <label>Address</label>
-                <input type="text" placeholder="mirpur, Dhaka" />
-              </div>
-              <div className="formInput">
-                <label>Country</label>
-                <input type="text" placeholder="BD" />
-              </div>
+              ))}
               <button>send</button>
             </form>
           </div>
