457307d1af62f66cdd92214cb7cc84316f6f0af2
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-05-07 18:19:56 +0600

    Create formSource.js

---
+++ admin_panel/src/pages/new/formSource.js
@@ -0,0 +1,76 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: "UserName",
+    type: "text",
+    placeholder: "john_doe",
+  },
+  {
+    id: 2,
+    label: "Name and Surname",
+    type: "text",
+    placeholder: "John Doe",
+  },
+  {
+    id: 3,
+    label: "Email",
+    type: "email",
+    placeholder: "john@example.com",
+  },
+  {
+    id: 4,
+    label: "Phone",
+    type: "text",
+    placeholder: "+880 123 4567",
+  },
+  {
+    id: 5,
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: 6,
+    label: "Address",
+    type: "text",
+    placeholder: "mirpur, Dhaka",
+  },
+  {
+    id: 7,
+    label: "Country",
+    type: "text",
+    placeholder: "BD",
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Apple Macbook Pro",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Computers",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+  {
+    id: 1,
+    label: "Stock",
+    type: "text",
+    placeholder: "in Stock",
+  },
+];
