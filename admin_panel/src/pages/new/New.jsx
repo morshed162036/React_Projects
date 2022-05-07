9fe20218260069d82fc96313a7dd670b0b5f10ab
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-05-07 12:55:46 +0600

    Update New.jsx

--- admin_panel/src/pages/new/New.jsx
+++ admin_panel/src/pages/new/New.jsx
@@ -1,6 +1,7 @@
 import "./new.scss"
 import Sidebar from "../../component/sidebar/Sidebar";
 import Navbar from "../../component/navbar/Navbar";
+import  DriveFolderUploadOutlinedIcon  from "@mui/icons-material/DriveFolderUploadOutlined";
 const New = () => {
   return (
     <div className="new">
@@ -17,7 +18,41 @@ const New = () => {
               alt="image_icon"
             />
           </div>
-          <div className="right">right</div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon"/>
+                </label>
+                <input type="file" id="file" style={{display:'none'}}/>
+              </div>
+              <div className="formInput">
+                <label>UserName</label>
+                <input type="text" placeholder="john_doe" />
+              </div>
+              <div className="formInput">
+                <label>Name and Surname</label>
+                <input type="text" placeholder="John Doe" />
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="text" placeholder="+880 123 4567" />
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password" />
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" placeholder="mirpur, Dhaka" />
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" placeholder="BD" />
+              </div>
+              <button>send</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
