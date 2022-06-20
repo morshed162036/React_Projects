6a2dcc92556e4b74c8fc2e3e3d551836c3b79874
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-06-20 19:03:53 +0600

    portfolio

--- portfolio/src/components/header/Header.jsx
+++ portfolio/src/components/header/Header.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import CTA from "./CTA";
 import "./header.css";
+import HeaderSocials from "./HeaderSocials";
+import ME from "../../assets/me-1.png";
+import { BsMouse } from "react-icons/bs";
+
 const Header = () => {
   return (
     <header>
@@ -9,6 +13,14 @@ const Header = () => {
         <h1>Morshed Ahmed</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+          <BsMouse className="mouse" />
+        </a>
       </div>
     </header>
   );
