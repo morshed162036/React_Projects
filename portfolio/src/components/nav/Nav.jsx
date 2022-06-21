7760319ebabe680be75983ed42b8ad524259264b
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-06-21 19:19:44 +0600

    nav

--- portfolio/src/components/nav/Nav.jsx
+++ portfolio/src/components/nav/Nav.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquareDetail } from "react-icons/bi";
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#"
+        className={activeNav === "#" ? "active" : ""}
+        onClick={() => setActiveNav("#")}
+      >
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <BiBook />
+      </a>
+      <a href="#services">
+        <RiServiceLine />
+      </a>
+      <a href="#contact">
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
