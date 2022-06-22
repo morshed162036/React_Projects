296bac34caa757065a4662135490defe1069342d
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-06-22 18:37:09 +0600

    experience

--- portfolio/src/components/about/About.jsx
+++ portfolio/src/components/about/About.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
-  return <section id="about">About</section>;
+  const [exp, setExp] = useState();
+  useEffect(() => {
+    var dString = new Date("May, 28, 2021");
+    let today = new Date();
+    today = today.getFullYear() - dString.getFullYear();
+    setExp(today);
+  });
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>{exp}+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid laudantium non commodi, accusantium repudiandae ad vero aut exercitationem sit culpa.
+          </p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- portfolio/src/components/nav/Nav.jsx
+++ portfolio/src/components/nav/Nav.jsx
@@ -12,20 +12,46 @@ const Nav = () => {
       <a
         href="#"
         className={activeNav === "#" ? "active" : ""}
-        onClick={() => setActiveNav("#")}
+        onClick={() => {
+          setActiveNav("#");
+        }}
       >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        className={activeNav === "about" ? "active" : ""}
+        onClick={() => {
+          setActiveNav("about");
+        }}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        className={activeNav === "experience" ? "active" : ""}
+        onClick={() => {
+          setActiveNav("experience");
+        }}
+      >
         <BiBook />
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        className={activeNav === "services" ? "active" : ""}
+        onClick={() => {
+          setActiveNav("services");
+        }}
+      >
         <RiServiceLine />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        className={activeNav === "contact" ? "active" : ""}
+        onClick={() => {
+          setActiveNav("contact");
+        }}
+      >
         <BiMessageSquareDetail />
       </a>
     </nav>
