ce8e3fa0fae83be89b9748c36f5751b8a0f28d38
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-06-25 16:51:22 +0600

    Update Contact.jsx

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>email@gmail.com</h5>
+            <a href="mailto:email@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>MT Studio</h5>
+            <a href="https://m.me/ernest.achiever" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>+123456789</h5>
+            <a
+              href="https:/api.whatsapp.com/send?phone+123456789"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTION */}
+        <form action="" method="post"></form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
